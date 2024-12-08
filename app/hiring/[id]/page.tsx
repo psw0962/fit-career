917775f3cb0cc1a020ca312b05772695abc9b82b
psw0962 <psw0962@naver.com>
2024-12-08 23:09:08 +0900

    Add react-kakao-maps-sdk and update hiring write page

--- app/hiring/[id]/page.tsx
+++ app/hiring/[id]/page.tsx
@@ -3,6 +3,9 @@
 import { EmblaOptionsType } from 'embla-carousel';
 import ThumbnailCarousel from '@/components/carousel/thumbnail-carousel';
 import { useGetHiring } from '@/actions/hiring';
+import Image from 'next/image';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import useKakaoLoader from '@/hooks/useKakaoLoader';
 
 const OPTIONS: EmblaOptionsType = {};
 
@@ -11,6 +14,8 @@ const HiringDetail = ({
 }: {
   params: { id: string };
 }): React.ReactElement => {
+  useKakaoLoader();
+
   const { data: hiringData } = useGetHiring(params.id);
 
   if (!hiringData || hiringData.length === 0) {
@@ -27,8 +32,28 @@ const HiringDetail = ({
         </div>
       )}
 
-      <div className="mt-10 flex gap-5">
-        <p>{hiringData[0].enterprise_name}</p>
+      <div className="mt-10 flex gap-5 items-center">
+        <div className="flex items-center gap-2">
+          <div className="relative w-8 h-8">
+            <Image
+              src={
+                hiringData[0]?.enterprise_logo
+                  ? hiringData[0]?.enterprise_logo
+                  : '/svg/logo.svg'
+              }
+              alt="enterprise logo"
+              fill
+              style={{ objectFit: 'cover' }}
+              priority
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+            />
+          </div>
+
+          <p className="text-xl underline underline-offset-4 decoration-[#000]">
+            {hiringData[0].enterprise_name}
+          </p>
+        </div>
         <div>∙</div>
         <p>{hiringData[0].short_address}</p>
         <div>∙</div>
@@ -47,7 +72,26 @@ const HiringDetail = ({
         />
       </div>
 
-      <div className="mt-10">카카오맵</div>
+      <div className="mt-16">
+        <Map
+          center={{
+            lat: 33.450701,
+            lng: 126.570667,
+          }}
+          style={{
+            width: '100%',
+            height: '450px',
+          }}
+          level={4}
+        >
+          <MapMarker
+            position={{
+              lat: 33.450701,
+              lng: 126.570667,
+            }}
+          />
+        </Map>
+      </div>
     </div>
   );
 };
