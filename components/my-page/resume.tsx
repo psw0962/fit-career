5d130b840caef41070e4c3954a74e068da1a3bf7
psw0962 <psw0962@naver.com>
2024-12-13 16:44:14 +0900

    feat: working on resume page

--- components/my-page/resume.tsx
+++ components/my-page/resume.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { useState } from 'react';
 
 interface LinkData {
@@ -28,7 +29,12 @@ const Resume = () => {
     setter: React.Dispatch<React.SetStateAction<T[]>>,
     defaultItem: T
   ) => {
-    setter((prev) => [...prev, defaultItem]);
+    setter((prev) => {
+      if (prev.length < 10) {
+        return [...prev, defaultItem];
+      }
+      return prev;
+    });
   };
 
   const updateField = <T,>(
@@ -54,51 +60,67 @@ const Resume = () => {
 
   return (
     <div className="flex flex-col mb-20 mt-10">
-      <div className="flex flex-col">
-        <p>이력서 사진</p>
+      <div className="flex flex-col mb-20">
+        <p className="text-2xl font-bold">이력서 사진</p>
 
         <input type="text" />
       </div>
 
-      <div className="flex flex-col">
-        <p>이름</p>
+      <div className="flex flex-col mb-20">
+        <p className="text-2xl font-bold">이름</p>
 
         <input type="text" />
       </div>
 
-      <div className="flex flex-col">
-        <p>전화번호</p>
+      <div className="flex flex-col mb-20">
+        <p className="text-2xl font-bold">전화번호</p>
 
         <input type="text" />
       </div>
 
-      <div className="flex flex-col">
-        <p>이메일</p>
+      <div className="flex flex-col mb-20">
+        <p className="text-2xl font-bold">이메일</p>
 
         <input type="text" />
       </div>
 
-      <div className="flex flex-col">
-        <p>간단 소개</p>
-        <p>
-          • 본인의 업무 경험을 기반으로 핵심 역량과 업무 스킬을 간단히 작성해
-          주세요.
-        </p>
-        <p>• 3~5줄로 요약하여 작성하는 것을 추천합니다.</p>
-        <p>
-          • 자신을 어필할 수 있는 포트폴리오가 있다면 링크도 첨부해 주세요.
-          ex)인스타그램, 블로그, 비포애프터 자료 등
-        </p>
+      <div className="flex flex-col mb-20">
+        <p className="text-2xl font-bold mb-2">간단 소개</p>
+
+        <div className="text-xs mb-7 sm:mb-2 p-2 bg-[#EAEAEC] rounded break-keep">
+          <p>
+            • 본인의 업무 경험을 기반으로 핵심 역량과 업무 스킬을 간단히 작성해
+            주세요.
+          </p>
+          <p>• 3~5줄로 요약하여 작성하는 것을 해요.</p>
+        </div>
 
         <input type="text" />
       </div>
 
-      <div className="flex flex-col">
-        <p>링크</p>
-        <p>• 자신을 어필할 수 있는 포트폴리오가 있다면 링크도 첨부해 주세요.</p>
+      <div className="flex flex-col mb-20">
+        <div className="flex gap-3 items-center mb-2">
+          <p className="text-2xl font-bold">링크</p>
+
+          <button
+            onClick={() => addField(setLinks, { title: '', url: '' })}
+            className="bg-[#4C71C0] text-white text-sm px-2 py-1 rounded w-fit"
+          >
+            + 링크 추가
+          </button>
+        </div>
+
+        <div className="text-xs mb-7 sm:mb-2 p-2 bg-[#EAEAEC] rounded break-keep">
+          <p>
+            • 자신을 어필할 수 있는 포트폴리오가 있다면 링크도 첨부해 주세요.
+            ex)인스타그램, 블로그, 비포애프터 자료 등
+            <br />• 최대 10개까지 등록할 수 있어요.
+          </p>
+        </div>
+
         {links.map((link, index) => (
-          <div key={index} className="flex items-center mb-2">
-            <div className="flex space-x-2 flex-grow">
+          <div key={index} className="flex items-center mb-10 relative sm:mb-2">
+            <div className="w-full flex flex-col items-center gap-2 sm:flex-row">
               <input
                 type="text"
                 placeholder="링크 제목"
@@ -111,8 +133,9 @@ const Resume = () => {
                     setLinks
                   )
                 }
-                className="w-[20%] p-2 border rounded"
+                className="w-full h-10 sm:w-[20%] p-2 border rounded"
               />
+
               <input
                 type="text"
                 placeholder="URL"
@@ -120,59 +143,112 @@ const Resume = () => {
                 onChange={(e) =>
                   updateField<LinkData>(index, 'url', e.target.value, setLinks)
                 }
-                className="w-[80%] p-2 border rounded"
+                className="w-full h-10 sm:w-[80%] p-2 border rounded"
               />
-            </div>
 
-            {links.length > 1 && (
-              <button
-                onClick={() => removeField(index, setLinks)}
-                className="bg-red-500 text-white px-3 py-2 rounded ml-2"
-              >
-                삭제
-              </button>
-            )}
+              {links.length > 1 && (
+                <Image
+                  src="/svg/close.svg"
+                  alt="close"
+                  width={15}
+                  height={15}
+                  className="w-auto h-4 cursor-pointer absolute right-0 top-[-20px] sm:static"
+                  onClick={() => removeField(index, setLinks)}
+                />
+              )}
+            </div>
           </div>
         ))}
-        <button
-          onClick={() => addField(setLinks, { title: '', url: '' })}
-          className="bg-blue-500 text-white px-4 py-2 rounded w-fit mt-2"
-        >
-          + 링크 추가
-        </button>
       </div>
 
-      <div className="flex flex-col">
-        <p>학력</p>
-        <p>• 최신순으로 작성해 주세요.</p>
+      <div className="flex flex-col mb-20">
+        <div className="flex gap-3 items-center mb-2">
+          <p className="text-2xl font-bold">학력</p>
+
+          <button
+            onClick={() => {}}
+            className="bg-[#4C71C0] text-white text-sm px-2 py-1 rounded w-fit"
+          >
+            + 학력 추가
+          </button>
+        </div>
+
+        <div className="text-xs mb-7 sm:mb-2 p-2 bg-[#EAEAEC] rounded break-keep">
+          <p>
+            • 최신순으로 작성해 주세요.
+            <br />• 최대 10개까지 등록할 수 있어요.
+          </p>
+        </div>
+
+        <input type="text" />
+      </div>
+
+      <div className="flex flex-col mb-20">
+        <div className="flex gap-3 items-center mb-2">
+          <p className="text-2xl font-bold">경력</p>
+
+          <button
+            onClick={() => {}}
+            className="bg-[#4C71C0] text-white text-sm px-2 py-1 rounded w-fit"
+          >
+            + 경력 추가
+          </button>
+        </div>
+
+        <div className="text-xs mb-7 sm:mb-2 p-2 bg-[#EAEAEC] rounded break-keep">
+          <p>
+            • 담당하신 업무 중 우선순위가 높은 업무를 선별하여 최신순으로 작성해
+            주세요. <br />• 신입의 경우, 직무와 관련된 대외활동, 인턴, 계약직
+            경력 등이 있다면 작성해주세요. <br />• 업무 또는 활동 시 담당했던
+            역할과 과정, 성과에 대해 자세히 작성해 주세요. <br />• 업무 성과는
+            되도록 구체적인 숫자 혹은 %로 표현해 주세요.
+            <br />• 최대 10개까지 등록할 수 있어요.
+          </p>
+        </div>
+
+        <input type="text" />
+      </div>
+
+      <div className="flex flex-col mb-20">
+        <div className="flex gap-3 items-center mb-2">
+          <p className="text-2xl font-bold">자격증</p>
+
+          <button
+            onClick={() => {}}
+            className="bg-[#4C71C0] text-white text-sm px-2 py-1 rounded w-fit"
+          >
+            + 자격증 추가
+          </button>
+        </div>
+
+        <div className="text-xs mb-7 sm:mb-2 p-2 bg-[#EAEAEC] rounded break-keep">
+          <p>
+            • 최신순으로 작성해 주세요.
+            <br />• 최대 10개까지 등록할 수 있어요.
+          </p>
+        </div>
 
         <input type="text" />
       </div>
 
       <div className="flex flex-col">
-        <p>경력</p>
-        <p>
-          • 담당하신 업무 중 우선순위가 높은 업무를 선별하여 최신순으로 작성해
-          주세요. <br />• 신입의 경우, 직무와 관련된 대외활동, 인턴, 계약직 경력
-          등이 있다면 작성해주세요. <br />• 업무 또는 활동 시 담당했던 역할과
-          과정, 성과에 대해 자세히 작성해 주세요. <br />• 업무 성과는 되도록
-          구체적인 숫자 혹은 %로 표현해 주세요. <br />• 커리어 조회 후 기업명이
-          실제와 다른 경우, 부서명/직책 란에 원하시는 기업명을 작성해 주세요.
-        </p>
+        <div className="flex gap-3 items-center mb-2">
+          <p className="text-2xl font-bold">수상 경력 및 활동</p>
 
-        <input type="text" />
-      </div>
+          <button
+            onClick={() => {}}
+            className="bg-[#4C71C0] text-white text-sm px-2 py-1 rounded w-fit"
+          >
+            + 추가
+          </button>
+        </div>
 
-      <div className="flex flex-col">
-        <p>자격증</p>
-        <p>• 최신순으로 작성해 주세요.</p>
-
-        <input type="text" />
-      </div>
-
-      <div className="flex flex-col">
-        <p>수상 경력 및 활동</p>
-        <p>• 최신순으로 작성해 주세요.</p>
+        <div className="text-xs mb-7 sm:mb-2 p-2 bg-[#EAEAEC] rounded break-keep">
+          <p>
+            • 최신순으로 작성해 주세요.
+            <br />• 최대 10개까지 등록할 수 있어요.
+          </p>
+        </div>
 
         <input type="text" />
       </div>
