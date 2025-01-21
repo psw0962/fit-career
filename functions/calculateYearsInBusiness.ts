32f2ac3c46f12a528ac1dea761b02d8518bd1c48
psw0962 <psw0962@naver.com>
2025-01-21 11:13:46 +0900

    fix: establishmentYear function

--- functions/calculateYearsInBusiness.ts
+++ functions/calculateYearsInBusiness.ts
@@ -5,5 +5,5 @@ export const calculateYearsInBusiness = (data: string) => {
 
   const establishmentYear = parseInt(data.split('-')[0], 10);
   const currentYear = new Date().getFullYear();
-  return Math.max(currentYear - establishmentYear, 0);
+  return Math.max(currentYear - establishmentYear + 1, 0);
 };
